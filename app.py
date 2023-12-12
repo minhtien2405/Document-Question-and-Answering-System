604b7df30dc3216fb589ac918e235befe55b3dcb
cristinpham <phamminhtien2405@gmail.com>
2023-12-13 02:53:03 +0700

    new commit

---
+++ app.py
@@ -0,0 +1,73 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings, OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+# from qdrant_client import QdrantClient
+# from langchain.vectorstores import Qdrant
+from langchain.vectorstores import FAISS
+import os
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrieverChain
+def get_pdf_text(pdf_docs):
+    text = ''
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(raw_text):
+    splitter = CharacterTextSplitter(
+        separator = '\n',
+        chunk_size = 1000,
+        chunk_overlap = 200,
+        length_function = len
+    )
+    text_chunks = splitter.split_text(raw_text)
+    return text_chunks
+
+def get_vector_store(text_chunks):
+    '''We can use Cohere-embed-multilingual-v3.0 in https://huggingface.co/cohere/embed-multilingual-v3.0 
+    embeddings = HuggingFaceEmbeddings(model_name='cohere/embed-multilingual-v3.0')
+    and vector_store = Qdrant(embeddings=embeddings)
+    '''
+    # embeddings = OpenAIEmbeddings(openai_api_key = os.getenv('OPENAI_API_KEY'))
+    embeddings = HuggingFaceInstructEmbeddings(model_name = 'hkunlp/instructor-xl')
+    vector_store = FAISS.from_texts(texts = text_chunks, embedding = embeddings)    
+    return vector_store
+
+def get_conversation_chain(vector_store):
+    memory = ConversationBufferMemory(memory_key = 'chat_history', return_message = True)
+    conversation_chain = ConversationalRetrieverChain(vector_store = vector_store, memory = memory)
+def main():
+    load_dotenv() 
+    st.set_page_config(page_title='Chat with multiple Documents', 
+                       page_icon= ':books:',
+                       layout='wide', 
+                       initial_sidebar_state='auto')
+
+    st.header('Chat with multiple Documents :books:')
+    st.text_input('Ask a question about the documents:')
+    
+    with st.sidebar:
+        st.subheader('Your Documents')
+        pdf_docs = st.file_uploader("Upload your documents here and click on 'Process':", type=['pdf', 'docx'], accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner('Processing your documents...'):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+                # st.write(raw_text)
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+                # st.write(text_chunks)
+                # create vector store
+                vector_store = get_vector_store(text_chunks)
+                # create conversation
+                conversation = get_conversation_chain(vector_store)
+
+
+
+
+if __name__ == '__main__':
+    main()
